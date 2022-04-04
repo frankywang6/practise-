e683007f514aae210fa7a87b072eaeb6cfdfde62
Franky Wang <jwan0454@uni.sydney.edu.au>
2022-04-04 12:53:05 +1000

    upload practice1

--- Program.cs
+++ Program.cs
@@ -1,11 +1,26 @@
 ﻿using System;
+
 namespace My_first_program
 {
     class Program 
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Change the appearance
+            Console.Title = "My first Programd";
+            Console.WindowHeight = 50;
+            //Get the conversation going
+            Console.WriteLine("A proud knight named...");
+
+            Console.ReadLine();
+            Console.WriteLine("...walked into a bar. At the counter he met a...");
+            Console.ReadLine();
+            Console.WriteLine("...who asked him what he wangted to drink?");
+            Console.ReadLine();
+            Console.WriteLine("...shouted the knight! The bartender shushed him quickly.");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("But it was too late... a dragon ate them both");
+            Console.ReadKey();
         }
     }
 }
