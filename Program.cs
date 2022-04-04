ac9307512c005b220ffc3555c78b40ab7f96962b
Franky Wang <jwan0454@uni.sydney.edu.au>
2022-04-04 10:58:32 +1000

    initialise the net

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace My_first_program
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
